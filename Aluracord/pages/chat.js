65754e394afc5f86918dd060dfad24bed85f60d4
raphael-ramos <raphael.ramos95@hotmail.com>
2022-01-25 19:52:43 -0300

    Aprendendo sobre states, hooks (useState, useRouter) e navegação entre telas

---
+++ Aluracord/pages/chat.js
@@ -0,0 +1,7 @@
+import { Text } from "@skynexui/components"
+
+export default function PaginaDoChat(){
+    return(
+        <div><h1>chat</h1></div>
+    )
+}
--- Aluracord/pages/index.js
+++ Aluracord/pages/index.js
@@ -1,34 +1,9 @@
 import appConfig from '../config.json';
-import {Box, Button, Text, TextField, Image} from '@skynexui/components'
+import {Box, Button, Text, TextField, Image} from '@skynexui/components';
+import {useRouter} from 'next/router';
+import React from 'react';
 
-function GlobalStyle(){
-    return (
-        <style global jsx>{`
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-        }
-        body {
-            font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-            min-height: 100vh;
-            display: flex;
-            flex: 1;
-        }
-        #__next {
-            flex: 1;
-        }
-        #__next > * {
-            flex: 1;
-        }
-        /* ./App fit Height */ 
-        `}</style>
-    );
-}
+
 
 function Titulo(props){
     // console.log(props);
@@ -59,11 +34,12 @@ function Titulo(props){
 }
 */}
 export default function PaginaInicial() {
-    const username = 'raphael-ramos';
+    //const username = 'raphael-ramos';
+    const [username, setUsername] = React.useState('raphael-ramos');
+    const roteamento = useRouter();
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -90,6 +66,12 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento){
+                infosDoEvento.preventDefault(); //previne do comportamento default de um submit (redirecionar o submit para uma página em caso do submit)
+                roteamento.push('/chat');
+                
+                // window.location.href = '/chat'; //redireciona para a página especificada (recarregando a página)
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -99,8 +81,9 @@ export default function PaginaInicial() {
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
+              
               <TextField
+              value
                 fullWidth
                 textFieldColors={{
                   neutral: {
@@ -109,6 +92,13 @@ export default function PaginaInicial() {
                     mainColorHighlight: appConfig.theme.colors.primary[500],
                     backgroundColor: appConfig.theme.colors.neutrals[800],
                   },
+                }}
+                value={username}
+                onChange={function (event){
+                  //onde está o valor?
+                  const valor = event.target.value
+                  //trocar o valor da variavel
+                  setUsername(valor)
                 }}
               />
               <Button
